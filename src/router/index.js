151c52d3d564ca37f571c25f89fd12a04cf073cc
BlackMoon-Angle <2717773566@qq.com>
2021-01-15 15:59:43 +0800

    完成分类参数的修改与删除功能

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
 // goods区
 import Cate from '../components/goods/Cate.vue'
+import Params from '../components/goods/Params.vue'
 
 Vue.use(VueRouter)
 
@@ -25,7 +26,8 @@ const routes = [
       { path: '/users', component: User },
       { path: '/rights', component: Rights },
       { path: '/roles', component: Roles },
-      { path: '/categories', component: Cate }
+      { path: '/categories', component: Cate },
+      { path: '/params', component: Params }
     ]
   }
 ]
